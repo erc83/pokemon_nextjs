85b0d29c5a36b20d39ea7a529802d24d548e9b49
erc83 <espinoza.eric@gmail.com>
2023-05-05 15:06:00 -0400

    aplicando nextui a la Card de pokemon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { NextPage, GetStaticProps } from "next"
+import { Grid, Card, Row, Text } from '@nextui-org/react';
 
 import { Layout } from "../components/layouts";
 import { pokeApi /* , .... */ } from "../../api";    // si tenemos mas apis las podemos llamar aqui
 import { PokemonListResponse, SmallPokemon } from "../../interfaces";
+ 
 
 interface Props {
   pokemons: SmallPokemon[];
@@ -16,16 +18,36 @@ export const HomePage: NextPage<Props>  = ( { pokemons } ) => {
   return (
     <>
       <Layout title="Listado de Pokemons">
-        <ul>
+        <Grid.Container gap={ 2 } justify="flex-start">
           {
             //pokemons.map((pokemon) => (
-            pokemons.map(({ id, name}) => (
-              <li key={ id }>
-                #{ id } - { name }
-              </li>
+            pokemons.map(({ id, name, img}) => (
+              
+              <Grid xs={ 6 } sm={ 3 } md={ 2 } xl={ 1 } key={ id }>
+                <Card isHoverable isPressable>
+                  <Card.Body css={{ p:1 }}>
+
+                    <Card.Image 
+                      src={ img }
+                      width="100%"
+                      height={ 140 }
+                    />
+                  </Card.Body>
+
+                  <Card.Footer>
+                    <Row justify="space-between">
+                      <Text transform="capitalize">{ name }</Text>
+                      <Text>#{ id }</Text>
+                    </Row>
+                  </Card.Footer>
+
+
+                </Card>
+              </Grid>
+              
             ))
           }
-        </ul>
+        </Grid.Container>
       </Layout>
     </>
   )
