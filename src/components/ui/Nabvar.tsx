a8617eeb415520aad8f3efa11946451d9e74315e
erc83 <espinoza.eric@gmail.com>
2023-05-07 01:10:15 -0400

    se añade pagina favoritos y se hace navegación al Home

--- src/components/ui/Nabvar.tsx
+++ src/components/ui/Nabvar.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import { Spacer, Text, useTheme } from "@nextui-org/react"
+import NextLink from 'next/link';  // aqui se renombra para que no choque con el Link de nextui
+import { /* Link, */ Spacer, Text, useTheme } from "@nextui-org/react"  // Link no fue necesario utilizarlo
 
 export const Nabvar = () => {
 
@@ -17,19 +18,26 @@ export const Nabvar = () => {
         backgroundColor: theme?.colors.gray100.value
     }}>
 
+      <NextLink href="/" style={{
+        display: 'flex'
+      }}>
         <Image 
             src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
             alt="icono de navbar"
             width={70}
             height={70}
         />
+          
+          <Text color='white' h2>P</Text>
+          <Text color='white' h3>okémon</Text>
 
-        <Text color='white' h2>P</Text>
-        <Text color='white' h3>okémon</Text>
+        </NextLink>
 
         <Spacer css={{ flex: 1 }} />
 
-        <Text color='white'>Favoritos</Text>
+        <NextLink href="/favorites">
+          <Text color='white'>Favoritos</Text>
+        </NextLink>
     
     </div>
   )
---
+++ src/pages/favorites/index.tsx
@@ -0,0 +1,16 @@
+import { Layout } from "@/components/layouts"
+
+
+const index = () => {
+  return (
+
+    <Layout title='pokemones favoritos'>
+
+        <h1>Hola pagina Favoritos</h1>
+
+
+    </Layout>
+  )
+}
+
+export default index
