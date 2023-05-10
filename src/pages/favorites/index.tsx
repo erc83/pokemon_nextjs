8a6a66cdc8093aefd48a5d376906562eaa11ec4d
erc83 <espinoza.eric@gmail.com>
2023-05-10 11:46:02 -0400

    pokemon en favoritesPage

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Grid, Card } from '@nextui-org/react';
 
 import { Layout } from "@/components/layouts"
 import NoFavorites from "@/components/ui/NoFavorites";
@@ -22,8 +23,32 @@ const FavoritesPage = () => {
 
     <Layout title='pokemones favoritos'>
 
-        <NoFavorites />
+        {
+          favoritePokemon.length === 0
+            ? (<NoFavorites />)
+            : (
+              <Grid.Container gap={ 2 } direction="row" justify="flex-start">
+                {
+                  favoritePokemon.map(( id ) => (
+                    <Grid xs={ 6 } sm={ 3 } xl={ 1 } key={ id }>
+                      <Card isHoverable isPressable css={{ padding: 10}}>  
+                        <Card.Image
+                          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ id }.svg`}
+                          width={'100%'}
+                          height={ 140 }
+                        />
+                      </Card>
+                    </Grid>
+                  ))
+                }
 
+
+              </Grid.Container>
+            )
+        }
+
+
+        
 
     </Layout>
   )
